b985dff62512b1127fbd517d62d89e586f933f40
Otto-Oskari <otto-oskari.suuronen@tuni.fi>
2025-01-16 12:18:09 +0200

    tehtava 4 tehty

--- vk2/ratkaisu_kierros1_teht4.cpp
+++ vk2/ratkaisu_kierros1_teht4.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
-int main(){
+void vaihda(int *osoitin1, int *osoitin2)
+{
+    int temp = *osoitin1;
+    *osoitin1 = *osoitin2;
+    *osoitin2 = temp;
+}
 
+void kaanna_taulukko(int *taulu, int koko)
+{
+    int *vasen = taulu;
+    int *oikea = taulu + koko - 1;
+
+    while (vasen < oikea)
+    {
+        vaihda(vasen, oikea);
+        vasen++;
+        oikea--;
+    }
+}
+
+int main()
+{
+    const int koko = 10;
+    int taulukko[koko];
+
+    for (int i = 0; i < koko; i++)
+    {
+        taulukko[i] = i + 1;
+    }
+
+    cout << "Alkuperäinen taulukko: ";
+    for (int i = 0; i < koko; i++)
+    {
+        cout << taulukko[i] << " ";
+    }
+    cout << endl;
+
+    kaanna_taulukko(taulukko, koko);
+
+    cout << "Taulukko käännettynä: ";
+    for (int i = 0; i < koko; i++)
+    {
+        cout << taulukko[i] << " ";
+    }
+    cout << endl;
 }
