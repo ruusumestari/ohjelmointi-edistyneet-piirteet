88d976f9b5944c31a9744d43c4fed366146ec6e4
Otto-Oskari <otto-oskari.suuronen@tuni.fi>
2025-01-16 12:23:07 +0200

    tehtava 2 tehty

--- vk2/ratkaisu_kierros1_teht2.cpp
+++ vk2/ratkaisu_kierros1_teht2.cpp
@@ -9,16 +9,17 @@ void swap_1(int *x, int *y)
     *x = *y;
     *y = temp;
 }
-void swap_2(int& a, int& b) {
+void swap_2(int &a, int &b)
+{
     int temp = a;
     a = b;
     b = temp;
 }
 int main()
 {
-    int num1 = 1,num2 = 2,num3 = 3,num4 = 4;
+    int num1 = 1, num2 = 2, num3 = 3, num4 = 4;
 
-    cout << "yksi = " << num1 << " kaksi = " << num2 << " ennen swap1." << endl; 
+    cout << "yksi = " << num1 << " kaksi = " << num2 << " ennen swap1." << endl;
     swap_1(&num1, &num2);
     cout << "yksi = " << num1 << " kaksi = " << num2 << " jälkeen swap1." << endl;
 
